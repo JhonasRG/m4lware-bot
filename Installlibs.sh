4bcaf1509d0d92f5b8ad1914998051a7f165daa9
Jhonas Gonçalves <34798848+JhonasRG@users.noreply.github.com>
2024-08-25 22:36:47 -0300

    Installlibs.sh

---
+++ Installlibs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install -y libnss3 libatk-bridge2.0-0 libx11-xcb1 libdrm2 libxcomposite1 libxcursor1 \
+    libxdamage1 libxrandr2 libgbm1 libpango-1.0-0 libasound2 libatk1.0-0 libatk-bridge2.0-0 \
+    libcups2 libxss1 libnss3 libgconf-2-4 libx11-xcb1 libxcb-xtest0
